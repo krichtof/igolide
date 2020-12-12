48e417b6662d6e4e1a9619b4411622af8a54ea1a
Christophe Robillard <geek@robiweb.net>
2020-12-12 22:59:17 +0100

    handle celebrate and warning events

--- code.py
+++ code.py
@@ -17,6 +17,7 @@ from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 from adafruit_led_animation.animation.sparkle import Sparkle
 from adafruit_led_animation.animation.sparklepulse import SparklePulse
+from adafruit_led_animation.animation.pulse import Pulse
 from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.group import AnimationGroup
 from adafruit_led_animation.animation import Animation
@@ -35,21 +36,11 @@ from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
-from adafruit_bluefruit_connect.packet import Packet
-from adafruit_bluefruit_connect.button_packet import ButtonPacket
-from adafruit_bluefruit_connect.color_packet import ColorPacket
-
 NUM_LEDS = 240                   # change to reflect your LED strip
-#NUM_LEDS = 10                   # change to reflect your LED strip
 NEOPIXEL_PIN = board.A1        # change to reflect your wiring
-# NEOPIXEL_PIN = board.NEOPIXEL
 
-# Declare a NeoPixel object on NEOPIXEL_PIN with NUM_LEDS pixels,
-# no auto-write.
-# Set brightness to max, we'll control it later in the code
 pixels = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_LEDS, brightness=1.0,
-                           auto_write=False,
-                           #pixel_order=(1,0,2,3) #uncomment if using RGBW NeoPixels
+                           auto_write=False
                            )
 
 
@@ -58,38 +49,15 @@ uart_service = UARTService()
 advertisement = ProvideServicesAdvertisement(uart_service)
 advertisement.complete_name = "DingDong"
 
-class RainbowFade(Animation):
-    ''' fades the entire strip through the whole spectrum '''
-    _color_index = 150 # choose start color (0-255)
-    def __init__(self, pixel_object, speed, name): # define animation
-        super().__init__(pixel_object, speed=speed, color=WHITE, name=name)
-
-    def draw(self): # draw the animation
-        ''' fades the entire strip through the whole spectrum '''
-        self.color = colorwheel(self._color_index + 1)
-        self._color_index = (self._color_index + 1) % 256
-        self.fill(self.color)
-
-# ANIMATION DEFINITIONS --
-#    create as many animations as you'd like and define their attributes here.
-#    They can be a single line or a group of animations - the groups will play
-#    at the same time, overlaid on top of each other.
 
 
-readingLight = Solid(pixels, color=0xFF7D13) #warm white color HEX code
 brightWhite = Solid(pixels, color=(150, 150, 150))
-rainbow = Rainbow(pixels, speed=0.1, period=10, step=0.5)
-rainbowfade = RainbowFade(pixels, speed=0.4, name="rainbowfade")
 powerup = RainbowComet(pixels, speed=0, tail_length=50, bounce=False)
+warning = Pulse(pixels, speed=0.1, color=AMBER, period=3)
 off = Solid(pixels, color=BLACK)
 
 #startup animation will play just once
 startup = AnimateOnce(powerup)
-
-#starrynight and fire are animation groups with layered effects.
-starrynight = AnimationGroup(
-    SparklePulse(pixels, speed=0.01, color=(0, 0, 150), period=1),
-    Comet(pixels, speed=0, tail_length=8, color=(150, 150, 150), bounce=False),)
 
 fire = AnimationGroup(
     Comet(pixels, speed=0, tail_length=1, color=BLACK),
@@ -102,18 +70,9 @@ fire = AnimationGroup(
 
 # Here is the animation playlist where you set the order of modes
 
-animations = AnimationSequence(
-        readingLight,
-        fire,
-        rainbow,
-        starrynight,
-        rainbowfade,
-        brightWhite,
-        auto_clear=True,
-        )
-
-
-
+animations = AnimationSequence(brightWhite, fire, warning)
+times = 20
+count = 0
 MODE = 0
 
 while True:
@@ -130,56 +89,24 @@ while True:
             if MODE == 2:
                 pass
             elif MODE == 1:
-                animations.animate()
+                brightWhite.animate()
     # Now we're connected
 
     while ble.connected:
-        if uart_service.in_waiting:
-            packet = Packet.from_stream(uart_service)
-            # Color Picker Functionality
-            if isinstance(packet, ColorPacket):
-                MODE = 2
-                # Set all the pixels to one color and stay there.
-                pixels.fill(packet.color)
-                pixels.show()
-            # Control Pad Functionality
-            elif isinstance(packet, ButtonPacket):
-                if packet.pressed:
-                    if packet.button == ButtonPacket.BUTTON_1:
-                        MODE = 1
-                        animations.activate(1)
-                        print("anim 1")
-                    elif packet.button == ButtonPacket.BUTTON_2:
-                        MODE = 1
-                        animations.activate(2)
-                        print("anim 2")
-                    elif packet.button == ButtonPacket.BUTTON_3:
-                        MODE = 1
-                        animations.activate(3)
-                        print("anim 3")
-                    elif packet.button == ButtonPacket.BUTTON_4:
-                        MODE = 1
-                        animations.activate(4)
-                        print("anim 4")
-                    # change the mode with right arrow
-                    elif packet.button == ButtonPacket.RIGHT:
-                        MODE = 1
-                        animations.next()
-                    elif packet.button == ButtonPacket.LEFT:
-                        MODE = 4
-                        off.animate()
-                    #change the brightness with up and down arrows
-                    elif packet.button == ButtonPacket.UP:
-                        pixels.brightness = pixels.brightness + 0.1
-                        pixels.show()
-                        if pixels.brightness > 1:
-                            pixels.brightness = 1
-                    elif packet.button == ButtonPacket.DOWN:
-                        pixels.brightness = pixels.brightness - 0.1
-                        pixels.show()
-                        if pixels.brightness < 0.1:
-                            pixels.brightness = 0.1
+        s = uart_service.readline().decode("utf-8").strip()
+        if s == 'celebrate':
+            count = 0
+            MODE = 1
+            animations.activate(1)
+            print("oh yeah ! celebrate !")
+        elif s == 'warning':
+            count = 0
+            MODE = 1
+            animations.activate(1)
+        if count > times:
+            MODE = 3
+            count = 0
+            off.animate()
         if MODE == 1:
             animations.animate()
-        if MODE == 4:
-            animations.freeze()
+            count += 1
