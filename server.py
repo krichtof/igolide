2d3a56b0898ce079b8ad03ded2fe6c9bfd587b86
Christophe Robillard <geek@robiweb.net>
2020-12-12 17:29:27 +0100

    fire rainbow animation when event occurs

--- server.py
+++ server.py
@@ -1,6 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+from adafruit_bluefruit_connect.color_packet import ColorPacket
+from adafruit_bluefruit_connect.button_packet import ButtonPacket
+
+ble = BLERadio()
+
+uart_connection = None
+
+if not uart_connection:
+    print("Trying to connect...")
+    for adv in ble.start_scan(ProvideServicesAdvertisement):
+        if UARTService in adv.services:
+            uart_connection = ble.connect(adv)
+            print("Connected")
+            break
+    ble.stop_scan()
+
 @app.route('/')
 def index():
     return "Notif Light anime votre salon lorsqu'un événement survient"
@@ -8,4 +27,11 @@ def index():
 @app.route('/events', methods=['POST'])
 def handle_event():
     print("coucou")
+
+    if uart_connection and uart_connection.connected:
+        uart_service = uart_connection[UARTService]
+        button_packet = ButtonPacket(b'1', True)
+        uart_service.write(button_packet.to_bytes())
+        print("animation 1 sent")
+
     return "Ok guy"
